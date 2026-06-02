75bb77a3bf8c1e0f568acbcf9843a104d30e5e96
Dale Gatchallan <dale@heypearl.io>
2026-06-02 20:27:30 +0800

    Add Google Analytics (G-3XS5TYSM7K)

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,6 +110,17 @@ export default function RootLayout({
             __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1120435960225787');fbq('track','PageView');`,
           }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3XS5TYSM7K"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-3XS5TYSM7K');`,
+          }}
+        />
       </body>
     </html>
   );
